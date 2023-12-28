1a07eeea732e3d4a34b49a7c73af573aaaf5ccbc
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2023-12-28 13:22:24 +0100

    added header with styles and open modal
    feat(Index.js): added open modal func
    feat(m-header-pug): added header
    feat(scss): added mixins for btn, added styles for header

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,3 +1,16 @@
-const index = () => {};
+import { event } from 'jquery';
+
+const index = () => {
+	const menuBtn = document.getElementById('menuBtn');
+	const navbarMenu = document.getElementById('navbarMenu');
+	const closeBtn = document.getElementById('closeBtn');
+
+	menuBtn.onclick = (event) => {
+		navbarMenu.style.display = 'block';
+	};
+	closeBtn.onclick = (event) => {
+		navbarMenu.style.display = 'none';
+	};
+};
 
 export default index;
