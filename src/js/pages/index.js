b8805369eebd55334eb9afc40ac8d147bb811bca
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2023-12-30 21:22:13 +0100

    updated modal js, code review fix
    added rem
    updated styles
    added hover
    added transition for modal
    added @media

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,16 +1,33 @@
 import { event } from 'jquery';
 
 const index = () => {
-	const menuBtn = document.getElementById('menuBtn');
-	const navbarMenu = document.getElementById('navbarMenu');
-	const closeBtn = document.getElementById('closeBtn');
+	const menuToggle = document.querySelector('.btn__menu');
 
-	menuBtn.onclick = (event) => {
-		navbarMenu.style.display = 'block';
-	};
-	closeBtn.onclick = (event) => {
-		navbarMenu.style.display = 'none';
-	};
+	menuToggle.addEventListener('click', () => {
+		const navbar = document.querySelector('.navbar');
+		navbar.classList.toggle('active');
+		menuToggle.classList.toggle('toggle');
+
+		if (navbar.classList.contains('active')) {
+			menuToggle.textContent = 'Close';
+		} else {
+			menuToggle.textContent = 'Menu';
+		}
+	});
+
+	document.addEventListener('keydown', (event) => {
+		if (event.code === 'Escape') {
+			const navbar = document.querySelector('.navbar'); // Змініть '.showcase' на клас вашого модального вікна
+			if (navbar.classList.contains('active')) {
+				navbar.classList.remove('active');
+				// Якщо є необхідність, змініть текст кнопки назад на 'Menu' або інший стандартний текст
+				const menuButton = document.querySelector('.btn__menu');
+				if (menuButton) {
+					menuButton.textContent = 'Menu';
+				}
+			}
+		}
+	});
 };
 
 export default index;
